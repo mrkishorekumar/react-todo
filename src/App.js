b86981255fcdfe5af0a658dc4b04d7c39eea3097
Kishore Kumar <mrkishorekumar18@gmail.com>
2022-11-11 10:11:06 +0530

    to do

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {  useState } from 'react'
+import './App.css'
 
 function App() {
+  const [todo, setTodo] = useState("")
+  const [list, setList] = useState([])
+
+
+  const formHandle = (e) => {
+    setTodo(e.target.value)
+  }
+
+  const formSubmit = (e) => {
+    e.preventDefault()
+    setTodo("")
+    setList((prev) => [...prev, todo])
+  }
+
+  const deleteFunction = (arrIndex) => {  
+    setList((prev)=>prev.filter((val,index)=>{
+      return arrIndex!==index
+    }))
+  }
+
+  const complete = (e) => {
+    e.target.style.textDecoration = "line-through"
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <main>
+        <h1>ToDo List</h1>
+        <form onSubmit={formSubmit}>
+          <input type="text" required placeholder='EnterTodo' value={todo} onChange={formHandle} />
+          <input type="submit" value="Add" />
+        </form>
+      </main>
+      <section>
+        {
+          (list.length!==0)?<table>
+          <tr>
+            <th>Todo</th>
+            <th>Options</th>
+          </tr>
+          {
+            list.map((val,index)=>{
+              return(
+                <tr key={index}>
+                  <td className='todo'>{val}</td>
+                  <td className='option'>
+                    <button className='completed' onClick={complete}>Completed</button>
+                    <button onClick={()=>deleteFunction(index)} className="delete">Delete</button>
+                  </td>
+                </tr>
+              )
+            })
+          }
+        </table>:<h1>You Have No Data!</h1>
+        }
+      </section>
+    </>
+  )
 }
 
-export default App;
+export default App
